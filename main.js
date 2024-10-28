1d138ab54aa2448bada4bfb4e209539657fd2cf4
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-28 14:54:29 +0100

    end commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('hello node js');
+
+const products = process.argv.slice(2)
+// products.forEach(product => {
+//     console.log(`${product}`);
+// });
+// console.log(process);
+console.log(process.env.PIN_KEY);
+// console.log(products);
+console.log(process.argv.slice(2));
+
+
+
+
